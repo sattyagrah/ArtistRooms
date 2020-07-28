739d0a9b60d2987b497e8c8b2a9bdaec36dfd4d8
Kumar satyanshu <kkumarsatyanshu2107@gmail.com>
2020-07-28 12:01:45 +0530

    Initial Commit

---
+++ js/auth.js
@@ -0,0 +1,62 @@
+function initApp(){
+    auth.onAuthStateChanged(user => {
+        if(user){
+            document.querySelector('#auth').style.display = "none";
+            document.querySelector('#logout').style.display = "block";
+            console.log('User Logged In : ', user.email)
+        }else{
+            console.log('User Logged Out!! ')
+        }
+    });
+}
+window.onload = function() {
+    initApp();
+  };
+
+const signupform = document.querySelector('#signup');
+signupform.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const email = signupform['email'].value;
+    const password = signupform['password'].value;
+
+    // console.log(email, password)
+
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        // console.log(cred)
+        const modal = document.querySelector('#id02');
+        modal.style.display = 'none';
+        signupform.reset();
+        document.querySelector('#auth').style.display = "none";
+        document.querySelector('#logout').style.display = "block";
+        window.location.replace("home.html")
+    })
+})
+
+const signinform = document.querySelector('#signin');
+signinform.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const email = signinform['login-email'].value;
+    const password = signinform['login-password'].value;
+
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        // console.log(cred.user)
+        document.querySelector('#auth').style.display = "none";
+        document.querySelector('#logout').style.display = "block";
+        const modals = document.querySelector('#id01');
+        modals.style.display = 'none';
+        signinform.reset(); 
+        window.location.replace("home.html") 
+    })
+})
+
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut().then(() => {
+        document.querySelector('#auth').style.display = "block";
+        document.querySelector('#logout').style.display = "none";
+        // console.log("User Signed Out")
+    })
+})
---
+++ js/auths.js
@@ -0,0 +1,109 @@
+function initApp(){
+    auth.onAuthStateChanged(user => {
+        if(user){
+            // users(user)
+            console.log('User Logged In : ', user.email)
+            document.querySelector('#names').innerHTML = user.email.split("@")[0];
+            document.querySelector('#auth').style.display = "none";
+            document.querySelector('#logout').style.display = "block";
+        }else{
+            // users([])
+            console.log('User Logged Out!! ')
+        }
+    });
+}
+
+window.onload = function() {
+    initApp();
+    db.collection('participants').get().then((snapshot) => {
+        // setUp(snapshot.docs)
+        snapshot.docs.forEach(doc => {
+            console.log(doc.id, " => ", doc.data(), doc.data().Uploaded_File)
+        })
+    })
+};
+
+const form = document.querySelector('#signin')
+form.addEventListener('submit', (e) => {
+    e.preventDefault()  
+    db.collection('participants').add({
+        Name : form.name.value,
+        // Username : document.querySelector('#names').textContent,
+        Phone_Number : form.phone.value,
+        Theme : form.theme.value,
+        Date : form.date.value,
+        Payment_Mode : form.pmode.value,
+        Transaction_ID : form.tid.value,
+        Uploaded_File : form.filename.value
+    })
+    document.querySelector('#participationforms').style.display = "none"
+    const uploadedfiles = document.querySelector('#myFile');
+    let files = uploadedfiles.files[0]
+    let storageref = storage.ref('images/' + files.name)
+    storageref.put(files)
+    storageref.put(files).then( snapshot => {
+        snapshot.ref.getDownloadURL().then(function(url){
+            let imageRes = url
+            console.log(imageRes)
+            const parent = document.querySelector('.feeds').querySelector('#new_posts')
+            let html = "" 
+            let li =  `
+            <div class="card col-md-12" style="width: 100%; height: 60%; max-width: 700px;">
+                    <div class="" style="padding:10px; text-align: left; width: auto;"><h5 class="card-text" style="font-family: 'Gotu', sans-serif;"> Tambe </h5>
+                    </div>
+                    <img id="upimgs" class="card-img-top" src=${imageRes} alt="Card image cap">
+                    <div class="card-body row">
+                        <div class="" style="float: left;"><a type="button" id="liked" onclick="document.getElementById('unliked').style.display = 'block'; document.querySelector('#no').textContent++;document.getElementById('liked').style.display = 'none' " ><i class="fa fa-heart" id="tup" style="color: coral;"></i></a><a type="button" style="display: none;" id="unliked" onclick="document.getElementById('liked').style.display = 'block'; document.getElementById('unliked').style.display = 'none'" ><i class="fa fa-heart" style="color: red"></i></a>
+                        </div>
+                        <div class="" style="padding-left:10px;"> <Span id="no"> 0 </Span>
+                        </div>
+                    </div>
+                </div>
+            `
+            html += li
+            parent.innerHTML = html
+            
+        })
+    })
+    console.log(files)
+    console.log(files.name)
+})
+
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut().then(() => {
+        document.querySelector('#auth').style.display = "block";
+        document.querySelector('#logout').style.display = "none";
+        window.location.replace("index.html")
+        // console.log("User Signed Out")
+    })
+})
+
+const ongoingmatches = document.querySelectorAll('#imgs');
+console.log(ongoingmatches)
+ongoingmatches.forEach(ele => {
+    ele.addEventListener('click', (e) => {
+        e.preventDefault()
+        document.querySelector('#participationforms').style.display = 'block';
+    })
+});
+
+const upcomingNotice = document.querySelectorAll('#uimgs');
+console.log(upcomingNotice)
+upcomingNotice.forEach(ele => {
+    ele.addEventListener('click', (e) => {
+        e.preventDefault()
+        document.querySelector('#upcomingnotice').style.display = 'block';
+    })
+})
+const previousInfo = document.querySelectorAll('#pimgs');
+console.log(previousInfo)
+previousInfo.forEach(ele => {
+    ele.addEventListener('click', (e) => {
+        e.preventDefault()
+        document.querySelector('#previousnotice').style.display = 'block';
+    })
+})
+
+
