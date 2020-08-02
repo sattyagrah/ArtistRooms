107cacc58334f8ae202af33e7aa6e430aacbc0c0
Kumar Satyanshu <kkumarsatyanshu2107@gmail.com>
2020-08-02 22:10:08 +0530

    Profile Photo Section added

--- js/auths.js
+++ js/auths.js
@@ -5,6 +5,7 @@ function initApp(){
             console.log('User Logged In : ', user.email)
             document.querySelector('#names').innerHTML = user.email.split("@")[0];
             forProfile();
+            profilePicture();
             document.querySelector('#auth').style.display = "none";
             document.querySelector('#logout').style.display = "block";
         }else{
@@ -17,43 +18,6 @@ function initApp(){
 window.onload = function() {
     initApp();
     getData();
-    // likes()
-    db.collection("participants").where("Username", "==", document.querySelector('#names').textContent)
-    .get()
-    .then(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
-            // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data());
-        });
-    })
-
-    // db.collection("participants").get().then(function(querySnapshot) {
-    //     querySnapshot.forEach(function(doc) {
-
-    //         const parent = document.querySelector('.feeds').querySelector('#new_posts')
-    //         let html = "" 
-    //         let li =  `
-    //         <div class="card col-md-12" style="width: 100%; height: 60%; max-width: 700px;">
-    //             <div class="" style="padding:10px; text-align: left; width: auto;"><h5 class="card-text" style="font-family: 'Gotu', sans-serif;"> ${doc.data().Username} </h5>
-    //             </div>
-    //             <img id="upimgs" class="card-img-top" src=${doc.data().Uploaded_File} alt="Card image cap">
-    //             <div class="card-body row">
-    //                 <div style="float: left;"><a type="button" id="liked" onclick="document.getElementById('unliked').style.display = 'block'; document.querySelector('#no').textContent++;document.getElementById('liked').style.display = 'none' " ><i class="fa fa-heart" id="tup" style="color: coral;"></i></a><a type="button" style="display: none;" id="unliked" onclick="document.getElementById('liked').style.display = 'block'; document.getElementById('unliked').style.display = 'none'" ><i class="fa fa-heart" style="color: red"></i></a>
-    //                 </div>
-    //                 <div style="padding-left:10px;"> <Span id="no"> 0 </Span>
-    //                 </div>
-    //             </div>
-    //         </div>
-    //             `
-    //         html += li
-    //         parent.innerHTML = html
-    //         // doc.data() is never undefined for query doc snapshots
-    //         console.log(doc.id, " => ", doc.data());
-    //     });
-    // }).catch(function(error) {
-    //     console.log("Error getting documents: ", error);
-    // });
-
     db.collection('participants').get().then((snapshot) => {
         // setUp(snapshot.docs)
         snapshot.docs.forEach(doc => {
